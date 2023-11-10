5c0fad7468edf1beceedee15f08f413113824d00
Maksim <playmakcim228@gmail.com>
2023-11-10 19:58:57 +0500

    start project 3

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # lyceum-lesson1
+
+## Активация виртуального окружения
+```bash
+python -m venv venv
+venv/Scripts/activate
+```
+
+## Установка зависимостей 
+```bash
+pip install -r requirements/prod.txt
+```
+
+## Установка зависимостей для разработки
+```commandline
+pip3 install -r requirements/dev.txt
+```
+
+## Установка зависимостей для тестирования
+```commandline
+pip3 install -r requirements/test.txt
+```
+
+## Настройка переменных окружения
+Скопируйте файл "config.env" в ".env", если нужно, отредактируйте значение переменных
+```commandline
+cp confing.env .env
+```
+
+## Запуск
+```commandline
+python manage.py runserver
+```
--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -10,27 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+import os
+import pathlib
+
+import dotenv
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv('DJANGO_SECRET', 'not_so_secret')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+DEBUG_ENV = os.getenv('DJANGO_DEBUG', 'true').lower()
+DEBUG = DEBUG_ENV in ('y', 'true', 'yes', '1', 't')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t9g81wh_&vdmvwc4no6t@+&3mj&ywa88=0cassci+g7w6q7mo('
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
 # Application definition
 
-INSTALLED_APPS = [  
+INSTALLED_APPS = [
     'homepage.apps.HomepageConfig',
     'about.apps.AboutConfig',
     'catalog.apps.CatalogConfig',
@@ -89,16 +88,28 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation'
+            '.UserAttributeSimilarityValidator'
+            ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation'
+            '.MinimumLengthValidator'
+            ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation'
+            '.CommonPasswordValidator'
+            ),
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation'
+            '.NumericPasswordValidator'
+            ),
     },
 ]
 
